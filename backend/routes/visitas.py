09623e273ac828ff759eba3b1bb1bffaf10d7c61
Alejandro <berroteranbas@gmail.com>
2025-03-21 18:48:28 -0300

    CRUD working perfectly

--- backend/routes/visitas.py
+++ backend/routes/visitas.py
@@ -49,35 +49,38 @@ async def get_visitors(db: Session = Depends(get_db)):
 
 # Route to get a visitor by rut
 # GET /visitas/{rut}
-# @router.get("/{rut}")
-# async def get_visitor(rut: str):
-#     for visitor in visitors_db:
-#         if visitor["rut"] == rut:
-#             return {"data": visitor}
-#     return {"mensaje": "Visitante no encontrado"}
+@router.get("/{rut}", response_model=VisitanteSchema)
+async def get_visitor(rut: str, db: Session = Depends(get_db)):
+    visitor  = db.query(Visitante).filter(Visitante.rut == rut).first()
+    if visitor:
+        return visitor
+    raise HTTPException(status_code=404, detail="Visitante no encontrado")
 
 # # Route to delete a visitor by rut
 # # DELETE /visitas/{rut}
-# @router.delete("/{rut}")
-# async def delete_visitor(rut: str):
-#     for visitor in visitors_db:
-#         if visitor["rut"] == rut:
-#             visitors_db.remove(visitor)
-#             return {"mensaje": "Visitante eliminado exitosamente"}
-#     return {"mensaje": "Visitante no encontrado"}
+@router.delete("/{rut}", status_code=204)
+async def delete_visitor(rut: str, db: Session = Depends(get_db)):
+    visitor = db.query(Visitante).filter(Visitante.rut == rut).first()
+    if visitor:
+        db.delete(visitor)
+        db.commit()
+        return {"mensaje": "Visitante eliminado exitosamente"}
+    raise HTTPException(status_code=404, detail="Visitante no encontrado")
 
 # # Route to update a visitor by rut
 # # PUT /visitas/{rut}
-# @router.put("/{rut}")
-# async def update_visitor(rut: str, visitor: Visitante):
-#     for visitor in visitors_db:
-#         if visitor["rut"] == rut:
-#             visitor["name"] = visitor.name
-#             visitor["age"] = visitor.age
-#             visitor["email"] = visitor.email
-#             visitor["phone"] = visitor.phone
-#             visitor["last_name"] = visitor.last_name
-#             visitor["userType"] = visitor.userType
-#             visitor["referring"] = visitor.referring
-#             return {"mensaje": "Visitante actualizado exitosamente"}
-#     return {"mensaje": "Visitante no encontrado"}
+@router.put("/{rut}")
+async def update_visitor(rut: str, visitor: VisitanteSchema, db: Session = Depends(get_db)):
+    visitor_db = db.query(Visitante).filter(Visitante.rut == rut).first()
+    if visitor_db:
+        visitor_db.name = visitor.name
+        visitor_db.age = visitor.age
+        visitor_db.email = visitor.email
+        visitor_db.phone = visitor.phone
+        visitor_db.last_name = visitor.last_name
+        visitor_db.userType = visitor.userType
+        visitor_db.referring = visitor.referring
+        db.commit()
+        db.refresh(visitor_db)
+        return visitor_db
+    raise HTTPException(status_code=404, detail="Visitante no encontrado")
